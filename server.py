2b342981340b273d712b1efd8fef3be936533b4b
Marina <marinacottrell@gmail.com>
2017-02-08 22:28:53 -0800

    added login and song saving

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect, request, jsonify
+from flask import Flask, render_template, redirect, request, jsonify, session, flash
 import os
 from parse_music import sample_song
+from model import User, Song, db, connect_to_db
 
 app = Flask(__name__)
 app.secret_key = os.environ['SECRET_KEY']
@@ -12,10 +13,48 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
-def login():
+@app.route('/login', methods=['POST'])
+def process_login():
 
-    return render_template('login.html')
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(username=username).first()
+    if user:
+        if user.password != password:
+            flash('Incorrect password!')
+            return redirect('/')
+    else:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+    session['user'] = user.username
+
+    return redirect('/')
+
+
+@app.route('/save', methods=['POST'])
+def save_song():
+
+    if 'user' not in session:
+        flash('Please log in to save your songs!')
+        return redirect('/')
+
+    name = request.form.get('name')
+    user = User.query.filter_by(username=session['user']).first()
+    try:
+        song = Song.query.order_by('song_id desc').limit(1).one()
+        song_id = song.song_id
+    except:
+        song_id = 0
+    song_id += 1
+    song_path = 'music/song' + str(song_id) + '.wav'
+    song = Song(user_id=user.user_id, song_path=song_path, name=name)
+    db.session.add(song)
+    db.session.commit()
+
+    return jsonify({})
 
 
 @app.route('/profile')
@@ -27,7 +66,8 @@ def profile():
 @app.route('/logout')
 def logout():
 
-    return redirect('index.html')
+    session.pop('user')
+    return redirect('/')
 
 
 @app.route('/process_song', methods=['POST'])
@@ -40,6 +80,7 @@ def song():
 
 if __name__ == '__main__':
 
+    connect_to_db(app)
     app.debug = True
     app.jinja_env.auto_reload = True
     app.run(port=5000, host='0.0.0.0')
