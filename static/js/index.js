dcbd16200cae0ca1345286e70cbad1d931f2f442
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-10 17:27:56 -0800

    moved javascript to separate files

---
+++ static/js/index.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var x = document.getElementById('audio');
+
+$('#play').on('click', function() {
+    x.play();
+});
+
+$('#pause').on('click', function() { 
+    x.pause();
+});
+
+$('#stop').on('click', function() { 
+    x.pause();
+    x.currentTime = 0;
+});
+
+$('#save').on('click', function() {
+    $('#save').attr('style', "display: none;");
+    $('#saved').attr('style', '');
+});
+
+$('#mel_submit').on('click', function(evt){
+    evt.preventDefault();
+    $('.b').attr('disabled', true);
+    var melody = $('#melody').val();
+    $.post('/process_song', {'melody': melody}, function(){
+
+        var source = 'static/song.wav?random=' + new Date().getTime();
+        $('#audio').attr('src', source);
+        $('.b').attr('disabled', false);
+        $('#saved').attr('style', "display: none;");
+        $('#save').attr('style', '');
+    });
+});
+
+
+$('#name_submit').on('click', function(evt){
+    evt.preventDefault();
+    var song_name = $('#name_input').val();
+    var data = {'name': song_name};
+    $.post('/save', data, function() {
+
+    });
+});
---
+++ static/js/profile.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var delete_song;
+
+$('.play').on('click', function(event) {
+    var song_id = event.target.parentNode.id;
+    var audio_id = song_id.replace('song', 'play');
+    var x = document.getElementById(audio_id);
+
+    x.play();
+});
+
+$('.pause').on('click', function(event) {
+    var song_id = event.target.parentNode.id;
+    var audio_id = song_id.replace('song', 'play');
+    var x = document.getElementById(audio_id);
+    x.pause();
+});
+
+$('.stop').on('click', function(event) {
+    var song_id = event.target.parentNode.id;
+    var audio_id = song_id.replace('song', 'play');
+    var x = document.getElementById(audio_id);
+    x.pause();
+    x.currentTime = 0;
+});
+
+$('.unsave').on('click', function(event) {
+    delete_song = event.target.parentNode.id;
+});
+
+$('#confirm_delete').on('click', function() {
+    var data = {'song_id': delete_song};
+    $.post('/delete_song', data, function() {
+        location.reload();
+    });
+});
+
