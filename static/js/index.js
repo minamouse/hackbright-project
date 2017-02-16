3c494a08d61ce6f5e2db09982763c28aef8b87f3
Marina <marinacottrell@gmail.com>
2017-02-15 22:34:13 -0800

    added accidentals to score

--- static/js/index.js
+++ static/js/index.js
@@ -1,89 +1,22 @@
-var draw_other_piece = function(notes, chords) {
-    var vf = new Vex.Flow.Factory({
-      renderer: {selector: 'boo', width: 500, height: 230}
-    });
-
-    var score = vf.EasyScore();
-
-    var width = 250;
-    var x = 5;
-
-    for (var i = 0; i < notes.length; i += 4) {
-
-        var note_list = [];
-        var chord_list = [];
-
-        var system = vf.System({ x: x, y: 5, width: width, spaceBetweenStaves: 10 });
-        for (var j = i; j < i+4; j++) {
-
-            if (j < notes.length) {
-                note_list.push(notes[j] + '/q');
-                chord_list.push('(' + chords[j].join(' ') + ')/q');
-            } else {
-                note_list.push('B4/q/r');
-                chord_list.push('D3/q/r');
-            }
-        }
-        var note_str = note_list.join(', ');
-        var chord_str = chord_list.join(', ');
-        console.log(note_str);
-        console.log(chord_str);
-
-        if (i === 0) {
-            system.addStave({
-              voices: [
-                score.voice(score.notes(note_str)),
-              ]
-            }).addClef('treble').addTimeSignature('4/4');
-
-            system.addStave({
-              voices: [
-                score.voice(score.notes(chord_str, {clef: 'bass'}))
-              ]
-            }).addClef('bass').addTimeSignature('4/4');
-
-            system.addConnector();
-
-        } else {
-
-            system.addStave({
-              voices: [
-                score.voice(score.notes(note_str)),
-              ]
-            });
-
-            system.addStave({
-              voices: [
-                score.voice(score.notes(chord_str, {clef: 'bass'}))
-              ]
-            });
-
-        }
-
-        x += width;
-    }
-
-    vf.draw();
-};
-
-
 
 var draw_piece = function (notes, chords) {
 
     var convert_note = function(note){
 
+        console.log(note);
         if (note.length == 2) {
             var key = note[0].toLowerCase() + '/' + note[1];
+            var accid = '0';
         } else {
             var key = note[0].toLowerCase() + '/' + note[2];
             var accid = note[1];
+            if (accid == '-') {
+                accid = 'b';
+            }
         }
-        return key;
+        return [key, accid];
 
-    }
-
-
-
+    };
 
 
     // stuff that just has to be done
@@ -94,27 +27,39 @@ var draw_piece = function (notes, chords) {
     var formatter = new VF.Formatter();
 
     var new_notes = [];
+    var note_accids = [];
     for (var i = 0; i < notes.length; i++){
-        new_notes.push(convert_note(notes[i]));
+        var results = convert_note(notes[i]);
+        new_notes.push(results[0]);
+        note_accids.push(results[1]);
     }
-
     var new_chords = [];
+    var chord_accids = [];
     for (var i = 0; i < chords.length; i++) {
         var c = chords[i];
 
-        var new_c = []
+        var new_c = [];
+        var accids = [];
         for (var j = 0; j < c.length; j++) {
-            new_c.push(convert_note(c[j]));
+            var results = convert_note(c[j]);
+            new_c.push(results[0]);
+            accids.push(results[1]);
         }
         new_chords.push(new_c);
+        chord_accids.push(accids);
     }
 
     var occurences = new_notes.map(function (e, i) {
         return [[e], new_chords[i]];
     });
 
+    var accidentals = note_accids.map(function (e, i) {
+        return [e, chord_accids[i]];
+    });
 
-    var x = occurences.length + occurences.length % 4;
+    console.log(occurences.length % 4);
+    var x = occurences.length + (4 - (occurences.length % 4));
+    console.log(x);
 
     var x_pos = 10;
     var size = 200;
@@ -129,7 +74,7 @@ var draw_piece = function (notes, chords) {
 
             var stave1 = new VF.Stave(x_pos, 20, size);
             var stave2 = new VF.Stave(x_pos, 110, size);
-            x_pos += size
+            x_pos += size;
         }
 
         var voice1 = new VF.Voice({num_beats: 4, beat_value: 4, resolution: Vex.Flow.RESOLUTION}).setMode(3);
@@ -139,13 +84,29 @@ var draw_piece = function (notes, chords) {
         var stave2_notes = [];
         for (var j = i; j < i+4; j++) {
 
-
+// addAccidental(2, new VF.Accidental("#")).
+// addAccidental(3, new VF.Accidental("n"))
+            
             if (j >= occurences.length) {
-                var note = new VF.GhostNote({clef: 'treble', keys: ['c/4'], duration: 'q'})
-                var chord = new VF.GhostNote({clef: 'bass', keys: ['c/4'], duration: 'q'})
+                console.log("i am adding a gnote");
+                var note = new VF.GhostNote({clef: 'treble', keys: ['c/4'], duration: 'q'});
+                console.log("i am adding a gchord");
+                var chord = new VF.GhostNote({clef: 'bass', keys: ['c/4'], duration: 'q'});
             } else {
-                var note = new VF.StaveNote({clef: 'treble', keys: occurences[j][0], duration: 'q'})
-                var chord = new VF.StaveNote({clef: 'bass', keys: occurences[j][1], duration: 'q'})
+                console.log("i am adding a note");
+                var note = new VF.StaveNote({clef: 'treble', keys: occurences[j][0], duration: 'q'});
+                console.log("i am adding a chord");
+                var chord = new VF.StaveNote({clef: 'bass', keys: occurences[j][1], duration: 'q'});
+
+                if (accidentals[j][0] !== '0') {
+                    note.addAccidental(0, new VF.Accidental(accidentals[j][0]));
+                }
+                for (var l = 0; l < accidentals[j][1].length; l++) {
+                    if (accidentals[j][1][l] !== '0') {
+                        chord.addAccidental(l, new VF.Accidental(accidentals[j][1][l]));
+                    }
+                }
+            
             }
 
             stave1_notes.push(note);
@@ -168,8 +129,7 @@ var draw_piece = function (notes, chords) {
 
     }
 
-
-}
+};
 
 
 
@@ -199,7 +159,7 @@ $('#stop').on('click', function() {
 
 $('#mel_submit').on('click', function(evt){
     evt.preventDefault();
-    $('#boo').replaceChild('<canvas width="500" height="230"></canvas>');
+    // $('#boo').replaceWith('<div id="boo" class="img-rounded" style="text-align: center; background: rgba(255,255,255,0.7); margin-left: auto; margin-right: auto;"><canvas width="500" height="230"></canvas></div>');
 
     $('.b').attr('disabled', true);
     var melody = $('#melody').val();
@@ -210,8 +170,10 @@ $('#mel_submit').on('click', function(evt){
 
         // var img_source = 'static/scores/song.svg?random=' + new Date().getTime();
         // $('img').attr('src', img_source);
+        $('canvas').attr('width', (Math.ceil(results.notes.length/4) * 200) + 80);
+        // console.log((Math.ceil(results.notes.length/4) * 200) + 50);
         draw_piece(results.notes, results.chords);
-        draw_other_piece(results.notes, results.chords);
+        // draw_other_piece(results.notes, results.chords);
 
         $('.b').attr('disabled', false);
         $('#saved').attr('style', "display: none;");
